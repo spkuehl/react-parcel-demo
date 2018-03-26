c17be23b308ff808239364764fa9296d8bfc451a
spkuehl <skuehlhorn@gmail.com>
2018-03-25 20:39:03 -0500

    Preliminary React app for displaying Book objects for bookshelf-web. Needs button for checkout/join waitlist.

--- src/index.js
+++ src/index.js
@@ -1,10 +1,38 @@
 import React from "react";
 import { render } from "react-dom";
-const App = () =>{
-  return (
+
+class BookButton extends React.Component {
+  render() {
     <div>
-      <h1>Hello Parcel</h1>
+      <button>Checkout</button>
     </div>
-  )
+  }
 };
-render(<App />, document.getElementById("app"));
+
+
+class BookDisplay extends React.Component {
+  render() {
+    return (
+      <div>
+        <div>{this.props.num}</div>
+        <div><img alt="Book Cover"/></div>
+        <div>
+          <h3>Title: {this.props.title}</h3>
+        </div>
+        <div>
+          <h4>Author: {this.props.author}</h4>
+        </div>
+        <div>
+          <h5>Published: {this.props.published}</h5>
+        </div>
+        <div>BUTTON</div>
+      </div>
+    )
+  }
+};
+
+render(<BookDisplay num='1'
+                    title='The Amber Spyglass'
+                    author='Phillip Pullman'
+                    published='2000'/>,
+                    document.getElementById('root0'));
